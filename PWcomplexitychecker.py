488c0e938bafdb391948b302f04d3e4e39ab3c72
Feven Demelash <101704341+PhevenDe@users.noreply.github.com>
2024-08-01 13:19:29 +0300

    Update PWcomplexitychecker.py

--- PWcomplexitychecker.py
+++ PWcomplexitychecker.py
@@ -1,29 +1,40 @@
 import re
 
+while True:
+    password = input("Enter password: ")
 
-def password_strength(password):
-    score = 0
-    length = len(password)
-    if 8 <= length <= 10:
-        score += 1
-    elif 11 <= length <= 15:
-        score += 2
-    elif length >= 16:
-        score += 3
+    def password_strength(password):
+        score = 0
+        length = len(password)
+        if length < 8:
+            score = 0
+        if 8 <= length <= 10:
+            score += 1
+        elif 11 <= length <= 15:
+            score += 2
+        elif length >= 16:
+            score += 3
 
-    if re.search(r"(?=.*[a-z]).*", password):
-        score += 1
-    if re.search(r"(?=.*[A-Z]).*", password):
-        score += 1
-    if re.search(r"(?=.*[0-9]).*", password):
-        score += 1
-    if re.search((r"(?=.*[!@#$%^&*]).*", password)):
-        score += 1
+        if re.search(r"(?=.*[a-z]).*", password):
+            score += 1
+        if re.search(r"(?=.*[A-Z]).*", password):
+            score += 1
+        if re.search(r"(?=.*[0-9]).*", password):
+            score += 1
+        if re.search(r"(?=.*[!@#$%^&*]).*", password):
+            score += 1
 
-    return score
+        if score < 2:
+            return "Weak", "Consider using a longer password with a mix of uppercase, lowercase, numbers, and special characters."
+        elif score < 4:
+            return "Poor", "Your password is still quite weak. Try to make it longer and include more character types."
+        else:
+            return "Strong", "Great, your password is strong and secure!"
 
+    password_strength_score, feedback = password_strength(password)
+    print(f"Password strength: {password_strength_score}")
+    print(f"Feedback: {feedback}")
 
-user_pw = input("Enter password")
-
-print(password_strength(user_pw))
-
+    again = input("Do you want to check another password? (y/n) ")
+    if again.lower() != 'y':
+        break
